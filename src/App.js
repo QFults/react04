f36071882d197fd59c7ab726953bece73ba955a8
Quinton Fults <coding.banjo@gmail.com>
2020-06-22 10:46:15 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {
+  Switch,
+  Link,
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom'
+import PageOne from './pages/PageOne'
+import PageTwo from './pages/PageTwo'
 
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Page One</Link>
+          <Link to="/pagetwo">Page Two</Link>
+        </nav>
+        <Switch>
+
+          <Route exact path="/">
+            <PageOne />
+          </Route>
+          <Route path="/pagetwo">
+            <PageTwo />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
